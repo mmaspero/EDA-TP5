8e7a389d0f0c2ce55f6dfefd046509b11d74b63b
mmaspero <mmaspero@itba.edu.ar>
2017-04-06 15:56:49 -0300

    Create GenericState.cpp

---
+++ EDA-TP5/GenericState.cpp
@@ -0,0 +1,47 @@
+#include "GenericState.h"
+
+
+GenericState* onRrq (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onWrq (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onData (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onLastData (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onAck(GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onLastAck (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onTimeout(GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onError (GenericEvent * ev)
+{
+    return NULL;
+}
+
+GenericState* onExit (GenericEvent * ev)
+{
+    return NULL;
+}
